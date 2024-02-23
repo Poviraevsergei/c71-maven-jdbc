5fd0b70ccb31f9f82e93dcf6c06eefebc55ad527
Poviraevsergei <poviraevsergei@gmail.com>
2024-02-23 16:06:23 +0300

    Jpa Crud

--- src/main/java/com/tms/Main.java
+++ src/main/java/com/tms/Main.java
@@ -1,10 +1,27 @@
 package com.tms;
 
+import com.tms.model.User;
 import com.tms.repository.UserRepository;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
         UserRepository userRepository = new UserRepository();
-        System.out.println(userRepository.truncateTelephoneTableFunction());
+        System.out.println(userRepository.findAll());
+
+/*        //Create
+        userRepository.createUser(user);
+
+        //Read
+        User user = userRepository.findById(108L);
+
+        //Update
+        user.setUsername("NEW_USERNAME");
+        userRepository.updateUser(user);
+
+        //Delete
+        userRepository.deleteUser(666L);*/
     }
 }
--- src/main/java/com/tms/repository/UserRepository.java
+++ src/main/java/com/tms/repository/UserRepository.java
@@ -1,184 +1,86 @@
 package com.tms.repository;
 
-import com.tms.User;
+import com.tms.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Timestamp;
-import java.sql.Types;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
-    private final static String SELECT_ALL_FROM_USERS = "SELECT * FROM users";
-    private final static String SELECT_USER_FROM_USERS = "SELECT * FROM users WHERE id=?";
-    private final static String INSERT_USER_INTO_USERS = "INSERT INTO users(id,username,user_password,created,changed,age)" +
-            " VALUES(DEFAULT,?,?,?,?,?)";
-    private final static String UPDATE_USER_INTO_USERS = "UPDATE users SET username=?, user_password=?, changed=?, age=? WHERE id=?";
-    private final static String REMOVE_USER_FROM_USERS = "DELETE FROM users WHERE id=?";
-    private final static String MOST_OLDER_USER = "{? = call max_old_in_the_users()}";
-    private final static String TRUNCATE_TELEPHONE_TABLE = "call truncate_telephone_table()";
-    private Connection connection = null;
-    //CRUD
 
+    private EntityManager entityManager = null;
 
     public UserRepository() {
-        try {
-            //1. Регистрация драйвера
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/c71_database", "postgres", "root");
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println(e);
-        }
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
+        entityManager = factory.createEntityManager();
     }
 
     public List<User> findAll() {
-        List<User> users = new ArrayList<>();
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(SELECT_ALL_FROM_USERS);
-
-            while (resultSet.next()) {
-                User user = parseUser(resultSet);
-                if (user.getId() != null) {
-                    users.add(user);
-                }
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return users;
+     //   Query query = entityManager.createNativeQuery("SELECT * FROM users", User.class);
+        Query query = entityManager.createQuery("FROM users", User.class); //users не таблица а название entity
+        return query.getResultList();
     }
 
-    //CRUD
-    //READ - достать обьект из БД
-    public User readUser(Long id) {
-        User user = null;
+    public User findById(Long id) {
         try {
-            PreparedStatement statement = connection.prepareStatement(SELECT_USER_FROM_USERS);
-            statement.setLong(1, id);
-            ResultSet resultSet = statement.executeQuery();
-
-            while (resultSet.next()) {
-                user = parseUser(resultSet);
-                if (user.getId() != null) {
-                    return user;
-                }
-            }
-        } catch (SQLException e) {
+            return entityManager.find(User.class, id);
+        } catch (Exception e) {
             System.out.println(e);
         }
-        return user;
+        return null;
     }
 
-    //CREATE - обавление в таблицу
     public boolean createUser(User user) {
         try {
-            PreparedStatement statement = connection.prepareStatement(INSERT_USER_INTO_USERS);
-            statement.setString(1, user.getUsername());
-            statement.setString(2, user.getUserPassword());
-            statement.setTimestamp(3, user.getCreated());
-            statement.setTimestamp(4, user.getChanged());
-            statement.setInt(5, user.getAge());
-            return statement.executeUpdate() == 1;
-        } catch (SQLException e) {
+            entityManager.getTransaction().begin();  //начинаем транзакцию
+            entityManager.persist(user); //метод для сохранения
+            entityManager.getTransaction().commit(); //комитаем(фиксируем изменения)
+            return true;
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
             System.out.println(e);
         }
         return false;
     }
 
-    //UPDATE - обновляет юзера
     public boolean updateUser(User user) {
         try {
-            PreparedStatement statement = connection.prepareStatement(UPDATE_USER_INTO_USERS);
-            statement.setString(1, user.getUsername());
-            statement.setString(2, user.getUserPassword());
-            statement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
-            statement.setInt(4, user.getAge());
-            statement.setLong(5, user.getId());
-            return statement.executeUpdate() == 1;
-        } catch (SQLException e) {
+            entityManager.getTransaction().begin();
+            entityManager.merge(user);
+            entityManager.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
             System.out.println(e);
         }
         return false;
     }
 
-    //DELETE - удаляет пользователя из БД
+    public boolean updateUserPassword(String password, Long id) {
+        try {
+            User user = entityManager.find(User.class, id);
+            user.setUserPassword(password);
+            entityManager.getTransaction().begin();
+            entityManager.merge(user);
+            entityManager.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            System.out.println(e);
+        }
+        return false;
+    }
+
     public boolean deleteUser(Long id) {
         try {
-            PreparedStatement statement = connection.prepareStatement(REMOVE_USER_FROM_USERS);
-            statement.setLong(1, id);
-            return statement.executeUpdate() == 1;
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return false;
-    }
-
-    public User parseUser(ResultSet resultSet) {
-        User user = new User();
-        try {
-            user.setId(resultSet.getLong("id"));
-            user.setUserPassword(resultSet.getString("user_password"));
-            user.setUsername(resultSet.getString("username"));
-            user.setCreated(resultSet.getTimestamp("created"));
-            user.setChanged(resultSet.getTimestamp("changed"));
-            user.setAge(resultSet.getInt("age"));
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return user;
-    }
-
-    public boolean checkTransaction() {
-        try {
-            connection.setAutoCommit(false);
-            PreparedStatement statementAge = connection.prepareStatement("UPDATE users SET age=100 WHERE id=13");
-            PreparedStatement statementUsername = connection.prepareStatement("UPDATE users SET username='USER_TRANSACTION' WHERE id=13");
-            PreparedStatement statementPassword = connection.prepareStatement("UPDATE users SET user_password='USER_PASS' WHERE id=13");
-            statementAge.executeUpdate();
-            statementUsername.executeUpdate();
-            statementPassword.executeUpdate();
-            connection.commit();
-            connection.setAutoCommit(true);
+            entityManager.getTransaction().begin();
+            entityManager.remove(entityManager.find(User.class, id));
+            entityManager.getTransaction().commit();
             return true;
-        } catch (SQLException e) {
-            System.out.println(e);
-            try {
-                connection.rollback();
-                connection.setAutoCommit(true);
-            } catch (SQLException exception) {
-                System.out.println(exception);
-            }
-
-        }
-        return false;
-    }
-
-    public String getUsernameOfTheMostOldUserFunction() {
-        String resultUsername = null;
-        try {
-            CallableStatement statement = connection.prepareCall(MOST_OLDER_USER);
-            statement.registerOutParameter(1, Types.VARCHAR);
-            statement.executeUpdate();
-            resultUsername = statement.getString(1);
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return resultUsername;
-    }
-
-    public boolean truncateTelephoneTableFunction() {
-        try {
-            CallableStatement statement = connection.prepareCall(TRUNCATE_TELEPHONE_TABLE);
-            statement.executeUpdate();
-            return true;
-        } catch (SQLException e) {
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
             System.out.println(e);
         }
         return false;

a62560c3ec0ad17fdbfdbf17b8aa31ffa9406bc5
Poviraevsergei <poviraevsergei@gmail.com>
2024-03-05 21:50:41 +0300

    Spring Core Annotation configuration

--- src/main/java/com/tms/Main.java
+++ src/main/java/com/tms/Main.java
@@ -3,12 +3,14 @@ package com.tms;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.tms") //что спринг будет вычитывать все классы через рефлексию начиная с того пакета где лежит этот класс(Main)
 public class Main {
     public static void main(String[] args) {
-        //1. создадим Spring Container (ApplicationContext) by Java
-        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-        Cap capBean = (Cap)context.getBean("getCapBean");
+        //1. создадим Spring Container (ApplicationContext) by Annotation
+        ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        Cap capBean = (Cap) context.getBean("cap");
         System.out.println(capBean);
     }
 }

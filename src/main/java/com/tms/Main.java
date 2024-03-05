c981089bb120c2c93cefe962e4dc742948ed2914
Poviraevsergei <poviraevsergei@gmail.com>
2024-03-05 21:39:45 +0300

    Spring Core Java configuration

--- src/main/java/com/tms/Main.java
+++ src/main/java/com/tms/Main.java
@@ -2,27 +2,13 @@ package com.tms;
 
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-// XML, Annotation, Java
-
-
-        //1. создадим Spring Container (ApplicationContext)
-        ApplicationContext context = new ClassPathXmlApplicationContext("my-settings.xml"); //Это когда настройки через XML
-        Cap capBean = (Cap) context.getBean("cap");
-        Cap capBeanSecond = (Cap) context.getBean("cap");
-
-        capBean.text = "NEW_ONE";
-
-        // cap это один и тот же бин!
-        System.out.println(capBean.text);
-        System.out.println(capBeanSecond.text);
-        System.out.println(capBean.hashCode());
-        System.out.println(capBeanSecond.hashCode());
-
-
-
+        //1. создадим Spring Container (ApplicationContext) by Java
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        Cap capBean = (Cap)context.getBean("getCapBean");
+        System.out.println(capBean);
     }
 }
